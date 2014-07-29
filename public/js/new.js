cb570f0432c19a616b921172500da52f2d136d04
lhnam298 <lhnam298@yahoo.com.vn>
2014-07-29 09:31:22 +0700

    no back when doing exam

--- public/js/new.js
+++ public/js/new.js
@@ -27,3 +27,45 @@ function toggleMe(a){
 	}
 	return true;
 }
+
+window.history.forward();
+function noBack() {
+	window.history.forward();
+}
+
+var countdownId = 0;
+
+function start(minute, sencond) {
+	//Start clock
+	countdownId = setInterval("countdown()", 1000);
+}
+
+function countdown(){
+	if(minute > 0){
+		if (second > 0) {
+			second	= second - 1;
+			document.getElementById('second').innerHTML = second;
+		}
+		if (second == 0) {
+			if (minute != 0) {
+				minute	= minute -1;
+				if (minute != 0)
+					second	= 59;
+				else
+					second	= 60;
+			}
+			document.getElementById('second').innerHTML = second;
+			document.getElementById('minute').innerHTML = minute;
+		}
+	}
+	if (minute == 0){
+		if (second > 0) {
+			second	= second - 1;
+			document.getElementById('second').innerHTML = second;
+		}
+		if (second == 0) {
+			clearInterval(countdownId);
+			document.exam.submit();
+		}
+	}
+}
